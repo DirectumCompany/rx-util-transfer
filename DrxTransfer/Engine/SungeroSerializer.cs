07bbaa58c05310c8a8e839ee9e4b0cb1c8f470be
Sukhanov_AA <sukhanov_aa@directum.ru>
2021-04-20 09:51:45 +0400

    Правка

--- DrxTransfer/Engine/SungeroSerializer.cs
+++ DrxTransfer/Engine/SungeroSerializer.cs
@@ -1,92 +1,136 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using Newtonsoft.Json;
 using Sungero.Domain.Client;
 using Sungero.Domain.Shared;
 
 namespace DrxTransfer.Engine
 {
-  /// <summary>
-  /// Сериализатор объекта.
-  /// </summary>
-  public abstract class SungeroSerializer
-  {
-    // TODO: убрать сеттер, передовать в конструкторе.
-    public string EntityName { get; set; }
-    public string EntityTypeName { get; set; }
-
-    public Dictionary<string, object> content;
-
     /// <summary>
-    /// Описание экспорта сущности.
+    /// Сериализатор объекта.
     /// </summary>
-    /// <param name="entity">Объект.</param>
-    /// <returns>Словарь с описанием реквизитов сущности.</returns>
-    protected virtual Dictionary<string, object> Export(IEntity entity)
+    public abstract class SungeroSerializer
     {
-      content["Card"] = entity;
-      return content;
+        public const string MetaSerializerTag = "Meta";
+        private readonly string AppendNewCardFormat = ",\n\t \"Card\" : \n\t\t{0}";
+        private readonly string AppendNewMetaFormat = ",\n\t  \"Meta\" : { \n\t\t{0} \n}";
+
+        // TODO: убрать сеттер, передовать в конструкторе.
+        public string EntityName { get; set; }
+        public string EntityTypeName { get; set; }
+
+        public Dictionary<string, object> content;
+
+        /// <summary>
+        /// Описание экспорта сущности.
+        /// </summary>
+        /// <param name="entity">Объект.</param>
+        /// <returns>Словарь с описанием реквизитов сущности.</returns>
+        protected virtual Dictionary<string, object> Export(IEntity entity)
+        {
+            content["Card"] = entity;
+            return content;
+        }
+
+        /// <summary>
+        /// Создание, заполнение реквизитов и сохранение сущности.
+        /// </summary>
+        /// <param name="jsonBody"></param>
+        public virtual void Import(Dictionary<string, object> jsonBody)
+        {
+
+        }
+
+        /// <summary>
+        /// Фильтрация выгружаемых записей.
+        /// </summary>
+        /// <param name="entities">Все сущности выгружаемого типа.</param>
+        /// <returns>Список отфильтрованных сущностей.</returns>
+        public virtual IEnumerable<IEntity> Filter(IEnumerable<IEntity> entities)
+        {
+            return entities;
+        }
+
+        /// <summary>
+        /// Сериализация объектов в json.
+        /// </summary>
+        /// <param name="filePath">Путь к файлу для записи.</param>
+        internal void Serialize(string filePath)
+        {
+            Log.Console.Info(string.Format("Сериализация объектов типа {0}", this.EntityTypeName));
+            this.content = new Dictionary<string, object>();
+
+            var type = Session.GetTypeNameGuid(Session.GetAppliedType(this.EntityTypeName)).GetTypeByGuid();
+
+            var entities = Session.Current.GetEntities(this.EntityTypeName).AsEnumerable();
+            entities = this.Filter(entities);
+
+            var entitiesCount = entities.Count();
+            Log.Console.Info(string.Format("Найдено {0} объектов", entitiesCount));
+
+            var result = new List<Dictionary<string, object>>();
+            var metaData = SetMetaData();
+
+            StringBuilder JSONBody = new StringBuilder();
+            JSONBody.Append("[\n");
+            var metaString = string.Empty;
+            foreach (var data in metaData.First().Value as Dictionary<string, object>)
+                metaString += string.Format("{0}:{0}\n", data.Key, data.Value);
+
+            metaString += "}";
+            Log.Console.Info(metaString);
+            JSONBody.Append(string.Format(AppendNewMetaFormat, metaString));
+
+            var index = 1;
+            int successful = 0, error = 0;
+            foreach (var entity in entities)
+            {
+                try
+                {
+                    Log.Console.Info(string.Format("Обработка записи {0} из {1}. ИД = {2}", index, entitiesCount, entity.Id));
+
+                    Log.Console.Info(string.Format("Сериализация объекта с ИД = {0} в json", entity.Id));
+                    string serializedObject = JsonConvert.SerializeObject(entity, Formatting.Indented, new JsonSerializerSettings
+                    {
+                        ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+                        ContractResolver = new SungeroEntitiesContractResolver()
+                    });
+                    Log.Console.Info(string.Format("Сериализация объекта с ИД = {0} завершена", entity.Id));
+
+                    JSONBody.Append(string.Format(AppendNewCardFormat, serializedObject));
+                    //result.Add(new Dictionary<string, object>() { this.Export(entity) });
+                    index++;
+                    successful++;
+                }
+                catch (Exception ex)
+                {
+                    error++;
+                    Log.Console.Error(ex, string.Format("Ошибка при серализации объекта в json. ИД = {0}", entity.Id));
+                }
+            }
+
+            JSONBody.Append("\n]");
+            using (StreamWriter sw = new StreamWriter(filePath, false, System.Text.Encoding.Default))
+            {
+                Log.Console.Info("Запись результата в файл");
+                sw.WriteLine(JSONBody);
+            }
+        }
+
+        private Dictionary<string,object> SetMetaData()
+        {
+            return new Dictionary<string, object>()
+            {   new KeyValuePair<string, object>(MetaSerializerTag,
+                new Dictionary<string, object>()
+                {
+                    new KeyValuePair<string, object>("EntityName", EntityName),
+                    new KeyValuePair<string, object>("EntityTypeName", EntityTypeName)
+                })
+            };
+
+        }
     }
-
-    /// <summary>
-    /// Создание, заполнение реквизитов и сохранение сущности.
-    /// </summary>
-    /// <param name="jsonBody"></param>
-    public virtual void Import(Dictionary<string, object> jsonBody)
-    {
-
-    }
-
-    /// <summary>
-    /// Фильтрация выгружаемых записей.
-    /// </summary>
-    /// <param name="entities">Все сущности выгружаемого типа.</param>
-    /// <returns>Список отфильтрованных сущностей.</returns>
-    public virtual IEnumerable<IEntity> Filter(IEnumerable<IEntity> entities)
-    {
-      return entities;
-    }
-
-    /// <summary>
-    /// Сериализация объектов в json.
-    /// </summary>
-    /// <param name="filePath">Путь к файлу для записи.</param>
-    internal void Serialize(string filePath)
-    {
-      Log.Console.Info(string.Format("Сериализация объектов типа {0}", this.EntityTypeName));
-      this.content = new Dictionary<string, object>();
-
-      var type = Session.GetTypeNameGuid(Session.GetAppliedType(this.EntityTypeName)).GetTypeByGuid();
-
-      var entities = Session.Current.GetEntities(this.EntityTypeName).AsEnumerable();
-      entities = this.Filter(entities);
-
-      var entitiesCount = entities.Count();
-      Log.Console.Info(string.Format("Найдено {0} объектов", entitiesCount));
-            
-      var result = new List<Dictionary<string, object>>();
-
-      var index = 1;
-      foreach (var entity in entities)
-      {
-        Log.Console.Info(string.Format("Обработка записи {0} из {1}. ИД = {2}", index, entitiesCount, entity.Id));
-        result.Add(new Dictionary<string, object>() { this.Export(entity) });
-        index++;
-      }
-
-      Log.Console.Info("Сериализация объектов в json");
-      string JSONBody = JsonConvert.SerializeObject(result, Formatting.Indented, new JsonSerializerSettings
-      {
-        ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
-        ContractResolver = new SungeroEntitiesContractResolver()
-      });
-
-      using (StreamWriter sw = new StreamWriter(filePath, false, System.Text.Encoding.Default))
-      {
-        Log.Console.Info("Запись результата в файл");
-        sw.WriteLine(JSONBody);
-      }
-    }
-  }
 }
--- DrxTransfer/Engine/TransferEngine.cs
+++ DrxTransfer/Engine/TransferEngine.cs
@@ -3,110 +3,122 @@ using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json;
 using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace DrxTransfer.Engine
 {
-  /// <summary>
-  /// Конвертация объектов.
-  /// </summary>
-  public class TransferEngine
-  {
-    #region Singletone Implementation
-
-    private static TransferEngine instance;
-    public static TransferEngine Instance
-    {
-      get
-      {
-        if (instance == null)
-          instance = new TransferEngine();
-        return instance;
-      }
-    }
-
-    #endregion
-
-    #region Методы
-
     /// <summary>
-    /// Запуск импорта данных.
+    /// Конвертация объектов.
     /// </summary>
-    /// <param name="filePath">Путь к файлу.</param>
-    public void Import(string filePath)
+    public class TransferEngine
     {
-      try
-      {
-        Log.Console.Info("Загрузка сериализатора");
-        var serializer = SerializersRepository.Instance.GetSerializerForEntityType(CommandLine.options.EntityType);
+        #region Singletone Implementation
 
-        if (serializer != null)
+        private static TransferEngine instance;
+        public static TransferEngine Instance
         {
-          string jsonText = string.Empty;
-          using (StreamReader sr = new StreamReader(filePath, System.Text.Encoding.Default))
-          {
-            Log.Console.Info("Чтение файла");
-            jsonText = sr.ReadToEnd();
-          }
-          var jsonBody = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(jsonText);
+            get
+            {
+                if (instance == null)
+                    instance = new TransferEngine();
+                return instance;
+            }
+        }
 
-          Log.Console.Info(string.Format("Сериализатор {0} успешно загружен", CommandLine.options.EntityType));
-          var index = 1;
-          var jsonItemsCount = jsonBody.Count();
-          foreach (var jsonItem in jsonBody)
-          {
+        #endregion
+
+        #region Методы
+
+        /// <summary>
+        /// Запуск импорта данных.
+        /// </summary>
+        /// <param name="filePath">Путь к файлу.</param>
+        public void Import(string filePath)
+        {
             try
             {
-              Log.Console.Info(string.Format("Запись {0} из {1}", index, jsonItemsCount));
-              index++;
-              serializer.Import(jsonItem);
+                Log.Console.Info("Загрузка сериализатора");
+
+                string jsonText = string.Empty;
+                using (StreamReader sr = new StreamReader(filePath, System.Text.Encoding.Default))
+                {
+                    Log.Console.Info("Чтение файла");
+                    jsonText = sr.ReadToEnd();
+                }
+                var jsonBody = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(jsonText);
+
+                SungeroSerializer serializer = null;
+                try
+                {
+                    var entityMeta = jsonBody.FirstOrDefault(z => z.Keys.Contains(SungeroSerializer.MetaSerializerTag))[SungeroSerializer.MetaSerializerTag] as JObject;
+                    serializer = SerializersRepository.Instance.GetSerializerForEntityType(entityMeta["EntityName"].ToString());
+                }
+                catch (Exception ex) 
+                {
+                    Log.Console.Error(string.Format("Не удалось получить сериализатор для файла: {0}. \n {1}", filePath, ex.Message));
+                    return;
+                }
+
+                if (serializer != null)
+                {
+                    Log.Console.Info(string.Format("Сериализатор {0} успешно загружен", CommandLine.options.EntityType));
+                    var index = 1;
+                    var jsonItemsCount = jsonBody.Count();
+                    foreach (var jsonItem in jsonBody)
+                    {
+                        try
+                        {
+                            Log.Console.Info(string.Format("Запись {0} из {1}", index, jsonItemsCount));
+                            index++;
+                            serializer.Import(jsonItem);
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Console.Error(ex);
+                            Log.Console.Error("Запись не создана");
+                            Session.Current.Dispose();
+                        }
+                    }
+                }
+                else
+                {
+                    throw new DllNotFoundException(string.Format("Сериализатор {0} не найден", CommandLine.options.EntityType));
+                }
             }
             catch (Exception ex)
             {
-              Log.Console.Error(ex);
-              Log.Console.Error("Запись не создана");
-              Session.Current.Dispose();
+                Log.Console.Error(ex);
             }
-          }
         }
-        else
+
+        /// <summary>
+        /// Запуск экспорта данных в json файл.
+        /// </summary>
+        /// <param name="filePath">Путь к файлу.</param>
+        public void Export(string filePath)
         {
-          throw new DllNotFoundException(string.Format("Сериализатор {0} не найден", CommandLine.options.EntityType));
+            try
+            {
+                Log.Console.Info("Загрузка сериализатора");
+                var serializer = SerializersRepository.Instance.GetSerializerForEntityType(CommandLine.options.EntityType);
+
+                if (serializer != null)
+                {
+                    Log.Console.Info(string.Format("Сериализатор {0} успешно загружен", CommandLine.options.EntityType));
+                    serializer.Serialize(filePath);
+                    return;
+                }
+                else
+                {
+                    throw new DllNotFoundException(string.Format("Сериализатор {0} не найден", CommandLine.options.EntityType));
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Console.Error(ex);
+            }
         }
-      }
-      catch (Exception ex)
-      {
-        Log.Console.Error(ex);
-      }
+
+        #endregion
     }
-
-    /// <summary>
-    /// Запуск экспорта данных в json файл.
-    /// </summary>
-    /// <param name="filePath">Путь к файлу.</param>
-    public void Export(string filePath)
-    {
-      try
-      {
-        Log.Console.Info("Загрузка сериализатора");
-        var serializer = SerializersRepository.Instance.GetSerializerForEntityType(CommandLine.options.EntityType);
-
-        if (serializer != null)
-        {
-          Log.Console.Info(string.Format("Сериализатор {0} успешно загружен", CommandLine.options.EntityType));
-          serializer.Serialize(filePath);
-          return;
-        }
-        else
-        {
-          throw new DllNotFoundException(string.Format("Сериализатор {0} не найден", CommandLine.options.EntityType));
-        }
-      }
-      catch (Exception ex)
-      {
-        Log.Console.Error(ex);
-      }
-    }
-
-    #endregion
-  }
 }
